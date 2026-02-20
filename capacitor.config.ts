14bf0e2aee31874624b0810487361587dfbdcb3d
Intesar Haider <intesarh7@gmail.com>
2026-02-20 19:56:34 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'in.snapkart.app',
-  appName: 'snapkart',
-  webDir: 'public'
-};
-
-export default config;
