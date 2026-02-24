c4f04f258880399b165259d38195e8baa5276bf5
intesarh7 <intesarh7@gmail.com>
2026-02-24 13:30:58 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,23 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
   compress: true,
+  turbopack: {},
+
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        protocol: "https", // 👈 keep as literal
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
@@ -15,4 +25,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig as any);
